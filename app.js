a720d24fc81f7049e9b43c544490052dcbbd7844
HamzLDN <hamchenhbf3@gmail.com>
2025-07-05 15:44:24 +0100

    few minor updates.

--- app.js
+++ app.js
@@ -2,7 +2,21 @@ const express = require('express')
 const app = express()
 const args = process.argv.slice(2);
 const [host, port] = [args[0], args[1]];
+console.log(host, port)
+app.use((req, res, next) => {
+    console.log(req.ip);
+    console.log(req.method)
+    console.log(req.originalUrl)
+    console.log(req.params)
+    console.log(req.query)
+    
+    next()
+})
 
-app.listen(port, () => {
+app.get("/", (req, res) => {
+    res.send("Hello");
+});
+
+app.listen(port, host, () => {
     console.log(`LISTENING ON -> http://${host}:${port}`);
 });
--- src/logdata/log_http.js
+++ src/logdata/log_http.js
@@ -1,13 +1,14 @@
 /* we are logging every requests and storing it. 
     I will make it send as an api once i get the web dashboard working */
-    
-function log_data(ip, method, url, query) { 
+const dict = {};
+function log_data(ip, method, url, params, query) { 
     const data_entry = dict.find(data_entry => data_entry.IP === ip);
 
-    const request = { Method:method,Url: url, Query: query};
+    const request = { Method:method,Url: url, Parameter: params, Query: query};
     if (!data_entry) {
         data_entry.Requests.push(request);
     } else {
         dict.push({ IP: ip,Requests: [request]});
     }
+    console.log(dict)
 }
