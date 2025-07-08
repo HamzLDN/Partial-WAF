4c97a45ff6031b019fcb851e1f70373a3f5a8e23
HamzLDN <75676416+HamzLDN@users.noreply.github.com>
2025-07-08 17:33:41 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const partial_middleware = require('./src/middleware/http_traffic')
+const partial_middleware = require('partial-protect')
 const app = express()
 const args = process.argv.slice(2);
 const [host, port] = [args[0], args[1]];
