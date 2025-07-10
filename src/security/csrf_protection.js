2f4766824423ad473195c047b6dff83fa439611c
HamzLDN <hamchenhbf3@gmail.com>
2025-07-10 15:09:30 +0100

    Update csrf_protection.js

--- src/security/csrf_protection.js
+++ src/security/csrf_protection.js
@@ -1,9 +1,21 @@
-function add_csrf_token(html_content) {
+function render_csrf(html_content) {
     const csrf_session  = csrf_gen(30)
     
+    const index = html_content.indexOf("<head>")
+    const before_head = html_content.slice(0, index);
+    const after_head = html_content.slice(index);
+    
+    return `${before_head}csrf-token="+${csrf_session['token']}"${after_head}`// i put the token in between the head
 }
 
 
+function generate_time(minute) {
+    return Date.now() + minute * 60 * 1000
+}
+
+function check_expiration(timestamp) {
+    return Date.now() > timestamp
+}
 
 function csrf_gen(token_length) {
     const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789=-+/';
@@ -11,5 +23,5 @@ function csrf_gen(token_length) {
     for (let i = 0; i < token_length; i++) {
       token += characters.charAt(Math.floor(Math.random() * characters.length));
     }
-    return {"token": token, "expiration": Date.now() + 30 * 60 * 1000}
+    return {"token": token, "expiration": generate_time(30)}
 }
