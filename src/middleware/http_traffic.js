babcffb180548f5397f0f39ea95b3a595bf649de
HamzLDN <hamchenhbf3@gmail.com>
2025-07-11 17:54:44 +0100

    JUNK
    Removed junk files from bootstrap studio template

--- src/middleware/http_traffic.js
+++ src/middleware/http_traffic.js
@@ -3,7 +3,6 @@ const csrf = require("../security/csrf_protection")
 const XssDetect = require('./xssdetect')
 const http = require('http');
 const express = require('express')
-const fetch = require('node-fetch');
 const path = require('path');
 const app = express()
 console.log( path.join(__dirname, '../assets'))
@@ -13,6 +12,7 @@ app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
 const { Server } = require('socket.io');
+const { info } = require('console');
 const server = http.createServer(app);
 app.use(express.urlencoded({ extended: true }));
 const io = new Server(server); 
@@ -23,38 +23,14 @@ var information = {
   'csrf': []
 }
 
-function handle_csrf_layer(req, res) {
-  const originalSend = res.send;
-  res.send = function (body) {
-    console.log("CSRF")
-    if (typeof body === 'string' && body.includes('<html')) {
-      const params = csrf.render_csrf(body)
-      body = params[0]
-
-      if (params[1] !== -1) {
-        information['csrf'].push({IP : req.ip, CSRF_TOKEN: params[1]['token'], EXPR: params[1]['expiration']})
-      }
-      console.log(information)
-    }
-    return originalSend.call(this, body);
-  };
-}
-
 function Partial_MiddleWare(req, res, next) {
-    if (req.method === "POST") {
-      try {
-        console.log("FOUND CSRF TOKEN -> ")
-        const is_valid = information['csrf'].some(x=>x['IP'] === req.ip && x['CSRF_TOKEN']===req.body['csrf'] && csrf.check_expiration(x['EXPR']))
-        if (!is_valid) {
-          console.log("CSRF_ID INCORRECT")
-          return res.status(401).send("CSRF_ID IS INCORRECT")
-        }
-      } catch (err){
-        return res.status(401).send("INTERNAL SERVER ERROR")
-      }
+  console.log(req.originalUrl)
+    if (req.method === "GET") {
+      csrf.handle_csrf_layer(req, res, information)
+    } else if (req.method === "POST") {
+      csrf.validate_csrf(req, res, information)
     }
     
-    handle_csrf_layer(req, res)
     const has_xss = XssDetect.containsXSS([req.method, req.originalUrl, JSON.stringify(req.headers)]);
     const new_headers = { ...req.headers };
     if (information['blacklist-ip'].some(x => x===req.ip)) {
@@ -64,6 +40,7 @@ function Partial_MiddleWare(req, res, next) {
         delete new_headers.cookie;
         console.log('Deleted cookies')
     }
+    console.log("HERE")
     http_log.log_data(req.ip, req.method, req.originalUrl, JSON.stringify(new_headers), has_xss, dict)[0];
     io.emit('network', dict);
     next();
@@ -102,6 +79,6 @@ io.on('connection', (socket) => {
 });
 
 server.listen(278, () => {
-    console.log('Dashboard server running on http://localhost:278');
-  });
+  console.log('Dashboard server running on http://localhost:278');
+});
 module.exports = Partial_MiddleWare;
--- src/security/csrf_protection.js
+++ src/security/csrf_protection.js
@@ -1,21 +1,30 @@
 function render_csrf(html) {
-    const token = csrf_gen(100)
 
-    const csrf_data = `\n<input type="hidden" name="csrf" value="${token['token']}" />`;
 
-    const new_csrf_data = insert_csrf(html);
-    console.log("++++++++++++++++")
-    console.log(new_csrf_data)
-
-    if (new_csrf_data === -1) {
+    const find_forms = find_multiple_index(html, "<form");
+    if (find_forms.length === 0) {
         return [html, -1]
-    } else {
-        return [html.slice(0, new_csrf_data) + csrf_data + html.slice(new_csrf_data), token]
     }
+    let new_csrf_data = html;
+    let offset = 0;
+
+    for (let x = 0; x < find_forms.length; x++) { // adds csrf for multiple form
+        const token = csrf_gen(100)
+        const csrf_data = `\n<input type="hidden" name="csrf" value="${token['token']}" />`;
+        let insert_index = insert_csrf(html, find_forms[x]);
+        if (insert_index === -1) continue;
+    
+        insert_index += offset;
+    
+        new_csrf_data =  new_csrf_data.slice(0, insert_index) +  csrf_data +  new_csrf_data.slice(insert_index);
+    
+        offset += csrf_data.length;
+    }
+    return [new_csrf_data, token];
+    
 }
 
-function insert_csrf(html) {
-    const form_start = html.indexOf("<form");
+function insert_csrf(html, form_start) {
     if (form_start === -1) return -1;
     let in_string = false;
     let quoteChar = null;
@@ -39,14 +48,65 @@ function insert_csrf(html) {
     return -1;
 }
 
+function handle_csrf_layer(req, res, information) {
+    const originalSend = res.send;
+    res.send = function (body) {
+      console.log("CSRF")
+      if (typeof body === 'string' && body.includes('<html')) {
+        const params = render_csrf(body)
+        body = params[0]
+  
+        if (params[1] !== -1) {
+          information['csrf'].push({IP : req.ip, CSRF_TOKEN: params[1]['token'], EXPR: params[1]['expiration']})
+        }
+        console.log(information)
+      }
+      return originalSend.call(this, body);
+    };
+}
 
+function find_multiple_index(html, string) {
+    let values = []
+    let find_str = ""
+    let counter = 0
+    for (let i = 0; i < html.length;i++) {
+        if (html[i] === string[counter]) {
+            find_str += html[i]
+            counter++;
+        } else {
+            counter = 0
+            find_str=""
+        }
+        if (find_str === string) {
+            values.push(i-string.length+1)
+        }
+    }
+    return values;
+}
+
+function validate_csrf(req, res, information) {
+    try {
+        const is_valid = information['csrf'].some(x=>x['IP'] === req.ip && x['CSRF_TOKEN'] === req.body['csrf'] && check_expiration(x['EXPR']))
+        if (!is_valid) {
+        console.log("CSRF_ID INCORRECT")
+        return res.status(401).send("CSRF_ID IS INCORRECT")
+        }
+    } catch (err){
+        console.log(err)
+        return res.status(50).send("INTERNAL SERVER ERROR")
+    }
+}
 
 function generate_time(minute) {
     return Date.now() + minute * 60 * 1000
 }
 
 function check_expiration(timestamp) {
-    return Date.now() > timestamp
+    return Date.now() < timestamp
+}
+
+function purge_csrf() {
+
 }
 
 function csrf_gen(token_length) {
@@ -58,5 +118,8 @@ function csrf_gen(token_length) {
     return {"token": token, "expiration": generate_time(30)}
 }
 module.exports = {
-    render_csrf, check_expiration
+    render_csrf, 
+    check_expiration, 
+    handle_csrf_layer,
+    validate_csrf
 }
